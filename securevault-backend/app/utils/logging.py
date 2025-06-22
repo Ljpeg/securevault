e7977a5ea50108ff9f29e1285677b07381f66692
La Tasha Pollard <latashajpollard@gmail.com>
2025-06-22 13:57:00 -0500

    Adds log helper

---
+++ securevault-backend/app/utils/logging.py
@@ -0,0 +1,7 @@
+from models.vault_log import VaultLog
+from extensions import db
+
+def log_action(user_id: int, item_id: int, action: str):
+  log = VaultLog(user_id=user_id, item=item_id, action=action)
+  db.session.add(log)
+  db.session.commit()
